45f925a97542ff5a6baf69e8b52667e9f68e1421
Chibuike Okoye <talk2chibyke@yahoo.com>
2022-09-11 01:22:46 +1000

    Rendered the weather info

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,10 @@ var button = document.getElementById('buttonid');
 var rootUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
 var APIkey = '8e7e0a08d319c6fd75c3deaef8e9b47b'
 var recentsearch = document.getElementById('recent-search');
-var CityNames = [];
 var CityName;
+var CityNames = [];
+
+
 
 button.onclick = function(event){
     event.preventDefault();
@@ -19,20 +21,42 @@ button.onclick = function(event){
         var recentsearchbutton = document.createElement('button');
         var valueselector = Cities[i];
         recentsearchbutton.append(valueselector);
+        recentsearchbutton.addEventListener('click', function(event){
+            getlatandlon(event.target.innerHTML)
+        })
         recentsearch.append(recentsearchbutton);
     }
 
-    getlatandlon();
+    getlatandlon(CityName);
 }
 
-var getlatandlon = function(){
-    fetch(rootUrl + CityName + '&appid=' + APIkey)
+var getlatandlon = function(city){
+    fetch(rootUrl + city + '&appid=' + APIkey)
     .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        var lat = data[0].lat
+        var lon = data[0].lon
+        
+        getweatherdata(lat,lon);
+    
       })
     }
+    
 
+
+var getweatherdata = function(lat,lon){
+    console.log(lat)
+    console.log(lon)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ lon +'&appid=' + APIkey)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (weather) {
+        console.log (weather)
+       
+      })
+
+}
     
